ba7d490cb8d3db6e5fe63605ada950ebfa71d2c0
Brandon Garcia <grandonleebarcia@gmail.com>
2021-05-24 11:42:31 -0700

    changed container fluid from xl to default, added rows and cols, and added link to start button

--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import ListOfGames from '../ListOfGames';
 const LandingPage: FC = ({ children }) => {
     return (
-        <Container className='h-75' fluid={'xl'}>
-            <Row className='justify-content-md-center h-50'>
+        <Container className='h-75' fluid >
+            <Row className='justify-content-center h-75'>
                 <Col className='align-self-center h-50 text-center' xl={6}>
-                    <h1>Brain Games</h1>
-                    <h3>Test your skills at these simple games to see how you match up</h3>
-                    <Button>Start</Button>
+                    <Row>
+                        <Col>
+                            <h1>Brain Games</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h4>Test your skills at these simple games to see how you match up</h4>
+                        </Col>
+                    </Row>
+                    <Row className='pt-4'>
+                        <Col>
+                            <Button as={Link} to='/targetPractice' >Start</Button>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
             <ListOfGames />
